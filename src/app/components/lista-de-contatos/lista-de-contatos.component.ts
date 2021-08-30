416bb36f18918ffc47038649af2f0f10bdb978e8
Raphael Seabra <77645109+seabraph@users.noreply.github.com>
2021-08-30 12:09:17 -0300

    Criado um model contato e atribuido-o a lista de contato

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos:Contato[] = [
+    {
+      nome: "Raphael Seabra",
+      email: "raphaeltibinha@gmail.com",
+      telefones: ["9999-3333"]
+    },
+    {
+      nome: "Ana Clara",
+      email: "anacjus@gmail.com",
+      telefones: ["2222-4444"]
+    },
+    {
+      nome: "Maria Ines",
+      email: "mariaines@gmail.com",
+      telefones: ["6666-1111"]
+    },
+    {
+      nome: "Rubens Justus",
+      email: "tibao@gmail.com",
+      telefones: ["5555-2222"]
+    },
+    {
+      nome: "Philippe Seabra",
+      email: "tibornex@gmail.com",
+      telefones: ["8888-2222"]
+    },
+  ]
 
   constructor() { }
 
